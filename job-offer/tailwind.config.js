d31f4f1bcdecc4bea11c98a2b1c2b35d30f06f70
prokylo <51948483+prokylo@users.noreply.github.com>
2021-12-05 16:45:06 +0800

    init: program configuration

---
+++ job-offer/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html"],  //去除index.html中没有用到的样式
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
