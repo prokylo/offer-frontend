cac5cddef4c8587f945dd5e5e05851705308b4fc
prokylo <51948483+prokylo@users.noreply.github.com>
2021-12-22 15:25:14 +0800

    update tailwindcss configuration

--- job-offer/src/views/PostPage.vue
+++ job-offer/src/views/PostPage.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>
-    {{ $route.params.postId }}
+  <div class="bg-gray-50 min-h-screen">
+    <div class="bg-white max-w-screen-xl mx-auto rounded shadow-lg">
+      <h2 class="px-8 pt-8 font-bold text-3xl">如何如何如何</h2>
+      <div class="author-bar flex p-8 items-center">
+        <img :src="avatar" class="w-12 h-12 mr-4 rounded-full" alt="">
+        <div class="author-info-box flex-col">
+          <h5 class="text-lg font-semibold">宜兴小火龙</h5>
+          <h6 class="text-gray-500">一个月前 · 阅读534</h6>
+        </div>
+        <button class="ml-auto border-2 border-green-400 bg-green-200 rounded-md px-4 w-30 text-green-500 h-12">
+          <span class="items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+            </svg>
+            关注
+          </span>
+        </button>
+      </div>
+      <DividerHorizontal class="w-11/12 mx-auto"/>
+      <div class="post-content">
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import avatar from '../assets/avatar.jpg'
+import DividerHorizontal from "../components/DividerHorizontal.vue";
 
 </script>
 
--- job-offer/tailwind.config.js
+++ job-offer/tailwind.config.js
@@ -1,21 +1,9 @@
 module.exports = {
-  // mode: "jit",
-  // purge: ["./index.html", "./src/**/*.{vue,js}"],  //去除index.html中没有用到的样式
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js,vue}"],
   theme: {
-    extend: {
-      width: {
-        '1/7': '14.2857143%',
-        '2/7': '28.5714286%',
-        '3/7': '42.8571429%',
-        '4/7': '57.1428571%',
-        '5/7': '71.4285714%',
-        '6/7': '85.7142857%'
-      }
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/typography')
+  ],
 }
